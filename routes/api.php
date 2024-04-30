67df4761d8b9db0694c1a363bbd9cbac3f1f5867
glenn2016 <glennleo242@gmail.com>
2024-04-30 11:39:59 +0000

    14 commit

--- routes/api.php
+++ routes/api.php
@@ -48,6 +48,7 @@ Route::group([
 Route::post('login', [AuthController::class ,'login']);
 Route::post('logout', [AuthController::class ,'logout']);
 Route::post('refresh', [AuthController::class ,'refresh']);
+Route::post('me', [AuthController::class ,'me']);
 
 
 //Participant
