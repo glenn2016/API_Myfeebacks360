8638282b6472ffd59efdaa4bc2602aa81f71a2a0
glenn2016 <glennleo242@gmail.com>
2024-05-17 14:23:35 +0000

    253 comit

--- routes/api.php
+++ routes/api.php
@@ -160,9 +160,10 @@ Route::middleware(['auth', 'role:participant'])->group(function () {
 
     Route::get('/evenements/admin',[EvenementController::class,'indexevenement'])->middleware('auth:api');
 
-    Route::get('evaluation/admin',[EvaluationController::class,'indexevaluation'])->middleware('auth:api');
+    Route::get('evaluations/admin',[EvaluationController::class,'indexevaluation'])->middleware('auth:api');
 
     
+
 
 
 });
